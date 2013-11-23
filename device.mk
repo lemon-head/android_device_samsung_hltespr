a016c3c05fe9a964c035a2fe0aeee0e1c4323c0b
KINGbabasula <kingbabasula@gmail.com>
2013-11-23 14:46:03 +0100

    Reorder

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fstab.qcom:recovery/root/fstab.qcom
+    $(LOCAL_PATH)/rootdir/fstab.qcom:recovery/root/fstab.qcom
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/libkeyutils.so:recovery/root/sbin/libkeyutils.so \
@@ -27,11 +27,11 @@ PRODUCT_COPY_FILES += \
 
 # SELinux
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/file_contexts:recovery/root/file_contexts \
-    $(LOCAL_PATH)/property_contexts:recovery/root/property_contexts \
-    $(LOCAL_PATH)/seapp_contexts:recovery/root/seapp_contexts \
-    $(LOCAL_PATH)/sepolicy:recovery/root/sepolicy \
-    $(LOCAL_PATH)/sepolicy_version:recovery/root/sepolicy_version
+    $(LOCAL_PATH)/selinux/file_contexts:recovery/root/file_contexts \
+    $(LOCAL_PATH)/selinux/property_contexts:recovery/root/property_contexts \
+    $(LOCAL_PATH)/selinux/seapp_contexts:recovery/root/seapp_contexts \
+    $(LOCAL_PATH)/selinux/sepolicy:recovery/root/sepolicy \
+    $(LOCAL_PATH)/selinux/sepolicy_version:recovery/root/sepolicy_version
 
 $(call inherit-product, build/target/product/full.mk)
 
