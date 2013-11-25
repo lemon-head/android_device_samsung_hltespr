5d35f9eea74ed7bdd779ff2502919bb0ff8757c3
KINGbabasula <kingbabasula@gmail.com>
2013-11-25 14:42:13 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
 # Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := SM-N900P
-=======
-# Release name
-PRODUCT_RELEASE_NAME := hltespr
->>>>>>> Update
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
-<<<<<<< HEAD
-$(call inherit-product, device/samsung/hltespr/full_hltespr.mk)
-
 # Device identifier. This must come after all inclusions
-=======
 $(call inherit-product, device/samsung/hltespr/device.mk)
 
 ## Device identifier. This must come after all inclusions
->>>>>>> Update
 PRODUCT_DEVICE := hltespr
 PRODUCT_NAME := cm_hltespr
 PRODUCT_BRAND := samsung
